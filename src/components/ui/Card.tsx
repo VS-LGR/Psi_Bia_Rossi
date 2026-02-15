51ea9664cb1fcdadb09e1b01b7bc64dcf65338f9
Lucas Gabriel Rodrigues <lucas@lgr-design.art>
2026-02-15 11:06:55 -0300

    Fix Vercel OnClick

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { cn } from '@/lib/utils'
 
-interface CardProps {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
   className?: string
 }
 
-export default function Card({ children, className }: CardProps) {
+export default function Card({ children, className, ...props }: CardProps) {
   return (
     <div
       className={cn(
         'bg-white rounded-xl shadow-lg p-6 transition-all duration-300 hover:shadow-xl',
         className
       )}
+      {...props}
     >
       {children}
     </div>
